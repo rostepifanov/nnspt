542d4644fcc26e1f0aabf638c49a1a7321112f0e
Epifanov Rostislav <rostepifanov@gmail.com>
2023-12-18 20:47:01 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Neural Network Signal Processing on Torch
+
+![Python version support](https://img.shields.io/pypi/pyversions/nnspt)
+[![PyPI version](https://badge.fury.io/py/nnspt.svg)](https://badge.fury.io/py/nnspt)
+[![Downloads](https://pepy.tech/badge/nnspt/month)](https://pepy.tech/project/nnspt?versions=0.0.*)
+
+NNSPT is a Python library for neural network signal processing on PyTorch.
+
+## Table of contents
+- [Authors](#authors)
+- [Installation](#installation)
+- [A simple example](#a-simple-example)
+- [Citing](#citing)
+
+## Authors
+[**Rostislav Epifanov** — Researcher in Novosibirsk]()
+
+## Installation
+Installation from PyPI:
+
+```
+pip install nnspt
+```
+
+Installation from GitHub:
+
+```
+pip install git+https://github.com/rostepifanov/nnspt
+```
+
+## A simple example
+```python
+import nnspt
+```
+
+## Citing
+
+If you find this library useful for your research, please consider citing:
+
+```
+@misc{epifanov2023ecgmentations,
+  Author = {Rostislav Epifanov},
+  Title = {NNSTP},
+  Year = {2023},
+  Publisher = {GitHub},
+  Journal = {GitHub repository},
+  Howpublished = {\url{https://github.com/rostepifanov/nnspt}}
+}
+```
---
+++ setup.py
@@ -0,0 +1,51 @@
+import setuptools
+
+from pathlib import Path
+
+INSTALL_REQUIRES = ['']
+
+def get_version():
+    locals_ = dict()
+
+    with open(Path(__file__).parent / 'nnspt' / '__version__.py') as f:
+        exec(f.read(), globals(), locals_)
+        return locals_['__version__']
+
+def get_long_description():
+    with open(Path(__file__).parent / 'README.md', encoding='utf-8') as f:
+        return f.read()
+
+setuptools.setup(
+    name='nnspt',
+    version=get_version(),
+    description='',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    author='Rostislav Epifanov',
+    author_email='rostepifanov@gmail.com',
+    license='MIT',
+    url='https://github.com/rostepifanov/nnspt',
+    packages=setuptools.find_packages(exclude=['tests']),
+    python_requires='>=3.7',
+    install_requires=INSTALL_REQUIRES,
+    extras_require={'tests': ['pytest']},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
