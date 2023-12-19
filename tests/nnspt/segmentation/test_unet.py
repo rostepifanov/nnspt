0494be5a927bee83d80968df385b7de86a4616ac
Rostislav Epifanov <rostepifanov@gmail.com>
2023-12-20 00:12:20 +0700

    add test on load encoder

--- tests/nnspt/segmentation/test_unet.py
+++ tests/nnspt/segmentation/test_unet.py
@@ -2,6 +2,7 @@ import pytest
 
 import torch
 
+from nnspt.blocks.encoders import Encoder
 from nnspt.segmentation.unet import Unet
 
 ENCODERS = [
@@ -20,8 +21,8 @@ ENCODERS = [
 
 @pytest.mark.unet
 @pytest.mark.segmentation
-@pytest.mark.parametrize('encoder', ENCODERS)
-def test_Unet_CASE_creation(encoder):
+@pytest.mark.parametrize('name', ENCODERS)
+def test_Unet_CASE_creation(name):
     IN_CHANNELS = 3
     OUT_CHANNELS = 2
     DEPTH = 5
@@ -30,7 +31,7 @@ def test_Unet_CASE_creation(encoder):
         in_channels=IN_CHANNELS,
         out_channels=OUT_CHANNELS, 
         depth=DEPTH,
-        encoder=encoder
+        encoder=name,
     )
 
     model.eval()
@@ -41,3 +42,27 @@ def test_Unet_CASE_creation(encoder):
     assert y.shape[0] == x.shape[0]
     assert y.shape[1] == OUT_CHANNELS
     assert y.shape[2] == x.shape[2]
+
+@pytest.mark.unet
+@pytest.mark.segmentation
+@pytest.mark.parametrize('name', ENCODERS)
+def test_Unet_CASE_load_encoder(name):
+    IN_CHANNELS = 3
+    OUT_CHANNELS = 2
+    DEPTH = 5
+
+    encoder = Encoder(
+        in_channels=IN_CHANNELS,
+        depth=DEPTH,
+        name=name,
+    )
+
+    model = Unet(
+        in_channels=IN_CHANNELS,
+        out_channels=OUT_CHANNELS,
+        depth=DEPTH,
+        encoder=name,
+    )
+
+    state = encoder.state_dict()
+    model.encoder.load_state_dict(state)
