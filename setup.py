a5612a3b13f7d1bdb738bfe3d7fce5b17d3b30e8
Rostislav Epifanov <rostepifanov@gmail.com>
2024-03-15 20:50:00 +0700

    update setup

--- setup.py
+++ setup.py
@@ -11,41 +11,52 @@ def get_version():
         exec(f.read(), globals(), locals_)
         return locals_['__version__']
 
+def get_short_description():
+    return 'A Python library for signal processing with PyTorch. Useful for machine learning.'
+
 def get_long_description():
     with open(Path(__file__).parent / 'README.md', encoding='utf-8') as f:
         return f.read()
 
+def get_python_min_version():
+    return (3, 7, 0)
+
+def get_python_min_version_str():
+    return '.'.join(map(str, get_python_min_version()))
+
 setuptools.setup(
     name='nnspt',
     version=get_version(),
-    description='',
+    description=get_short_description(),
     long_description=get_long_description(),
     long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(exclude=['tests']),
+    install_requires=INSTALL_REQUIRES,
+    extras_require={'tests': ['pytest']},
+    url='https://github.com/rostepifanov/nnspt',
+    download_url='https://github.com/rostepifanov/nnspt/tags',
     author='Rostislav Epifanov',
     author_email='rostepifanov@gmail.com',
     license='MIT',
-    url='https://github.com/rostepifanov/nnspt',
-    packages=setuptools.find_packages(exclude=['tests']),
-    python_requires='>=3.7',
-    install_requires=INSTALL_REQUIRES,
-    extras_require={'tests': ['pytest']},
+    python_requires=f'>={get_python_min_version_str()}',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
+        'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
         'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ] + [
+        f'Programming Language :: Python :: 3.{i}'
+            for i in range(get_python_min_version()[1], 12)
+    ],
+    keywords='nnspt, augmentation, deep learning',
 )
